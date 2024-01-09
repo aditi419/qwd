c441784c0850314c7f7a9219b2815c4a70b6882a
aditi419 <76972940+aditi419@users.noreply.github.com>
2024-01-08 20:57:17 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,46 @@
+import socket
+from threading import Thread
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+IP_ADDRESS = '127.0.0.1'
+PORT = 8050
+SERVER = None
+BUFFER_SIZE = 4096
+clients = {}
+
+def acceptConnections():
+    global SERVER
+    global clients
+    
+    while True:
+        client, addr = SERVER.accept()
+        prunt(client.addr)\
+
+def setup():
+    print("\n\t\t\t\t\t\tIP MESSENGER\n")
+
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+
+    acceptConnections()
+
+def ftp():
+    global IP_ADDRESS
+
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("lftpd","lftpd",".",perm="elradfmw")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    ftp_server = FTPServer((IP_ADDRESS,21),handler)
+    ftp_server.serve_forever()
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
+
+ftp_thread = Thread(target=ftp)
+ftp_thread.start()
